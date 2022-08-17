c996d2b5fe41d7a38cd789e5f73018cc27b01fa7
jdbs9514 <jdbs9514@gmail.com>
2022-08-17 17:49:08 -0500

    add the calculate and operate files

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Components from './components/calculator';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Components/>
+    )
+  }
+}
+
+export default App;
