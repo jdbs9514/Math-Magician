a4d42f9fb3b3edef885f9e14b27f2c6fc95c1d66
jdbs9514 <jdbs9514@gmail.com>
2022-08-18 18:09:59 -0500

    add events and fix linters

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
+import calculate from '../logic/calculate';
 
 class Components extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleClick = this.handleClick.bind(this);
     }
+    handleClick(event) {
+        this.setState((state) => calculate(state, event.target.textContent));
+      }
+
     render() {
+        const { next, total } = this.state;
         return (
             <div className='container'>
                 <form>
-                    <input type="text"/>
+                    <input type="text" value={next || total || 0}/>
                 </form>
                 <div className='keypad'>
-                    <button type='button' className='firstline'>AC</button>
-                    <button type='button' className='firstline'>+/-</button>
-                    <button type='button' className='firstline'>%</button>
-                    <button type='button' className='extra'>&divide;</button>
-                    <button type='button' className='secondline'>7</button>
-                    <button type='button' className='secondline'>8</button>
-                    <button type='button' className='secondline'>9</button>
-                    <button type='button' className='extra'>x</button>
-                    <button type='button' className='thirdline'>4</button>
-                    <button type='button' className='thirdline'>5</button>
-                    <button type='button' className='thirdline'>6</button>
-                    <button type='button' className='extra'>&ndash;</button>
-                    <button type='button' className='fourthline'>1</button>
-                    <button type='button' className='fourthline'>2</button>
-                    <button type='button' className='fourthline'>3</button>
-                    <button type='button' className='extra'>+</button>
-                    <button type='button' className='cero'>0</button>
-                    <button type='button' className='dot'>.</button>
-                    <button type='button' className='equal'>=</button>
+                    <button type='button' className='firstline' onClick={this.handleClick}>AC</button>
+                    <button type='button' className='firstline' onClick={this.handleClick}>+/-</button>
+                    <button type='button' className='firstline' onClick={this.handleClick}>%</button>
+                    <button type='button' className='extra' onClick={this.handleClick}>&divide;</button>
+                    <button type='button' className='secondline' onClick={this.handleClick}>7</button>
+                    <button type='button' className='secondline' onClick={this.handleClick}>8</button>
+                    <button type='button' className='secondline' onClick={this.handleClick}>9</button>
+                    <button type='button' className='extra' onClick={this.handleClick}>x</button>
+                    <button type='button' className='thirdline' onClick={this.handleClick}>4</button>
+                    <button type='button' className='thirdline' onClick={this.handleClick}>5</button>
+                    <button type='button' className='thirdline' onClick={this.handleClick}>6</button>
+                    <button type='button' className='extra' onClick={this.handleClick}>-</button>
+                    <button type='button' className='fourthline' onClick={this.handleClick}>1</button>
+                    <button type='button' className='fourthline' onClick={this.handleClick}>2</button>
+                    <button type='button' className='fourthline' onClick={this.handleClick}>3</button>
+                    <button type='button' className='extra' onClick={this.handleClick}>+</button>
+                    <button type='button' className='cero' onClick={this.handleClick}>0</button>
+                    <button type='button' className='dot' onClick={this.handleClick}>.</button>
+                    <button type='button' className='equal' onClick={this.handleClick}>=</button>
                 </div>
             </div>
         )
