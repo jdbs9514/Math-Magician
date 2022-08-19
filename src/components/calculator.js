3c7e91cdc3e3eadcffe0d56347efe99624edd3db
jdbs9514 <jdbs9514@gmail.com>
2022-08-19 02:22:21 -0500

    Add Hooks instead classes on react

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,47 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Components extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick(event) {
-        this.setState((state) => calculate(state, event.target.textContent));
-      }
-
-    render() {
-        const { next, total } = this.state;
-        return (
-            <div className='container'>
-                <form>
-                    <input type="text" value={next || total || 0}/>
-                </form>
-                <div className='keypad'>
-                    <button type='button' className='firstline' onClick={this.handleClick}>AC</button>
-                    <button type='button' className='firstline' onClick={this.handleClick}>+/-</button>
-                    <button type='button' className='firstline' onClick={this.handleClick}>%</button>
-                    <button type='button' className='extra' onClick={this.handleClick}>&divide;</button>
-                    <button type='button' className='secondline' onClick={this.handleClick}>7</button>
-                    <button type='button' className='secondline' onClick={this.handleClick}>8</button>
-                    <button type='button' className='secondline' onClick={this.handleClick}>9</button>
-                    <button type='button' className='extra' onClick={this.handleClick}>x</button>
-                    <button type='button' className='thirdline' onClick={this.handleClick}>4</button>
-                    <button type='button' className='thirdline' onClick={this.handleClick}>5</button>
-                    <button type='button' className='thirdline' onClick={this.handleClick}>6</button>
-                    <button type='button' className='extra' onClick={this.handleClick}>-</button>
-                    <button type='button' className='fourthline' onClick={this.handleClick}>1</button>
-                    <button type='button' className='fourthline' onClick={this.handleClick}>2</button>
-                    <button type='button' className='fourthline' onClick={this.handleClick}>3</button>
-                    <button type='button' className='extra' onClick={this.handleClick}>+</button>
-                    <button type='button' className='cero' onClick={this.handleClick}>0</button>
-                    <button type='button' className='dot' onClick={this.handleClick}>.</button>
-                    <button type='button' className='equal' onClick={this.handleClick}>=</button>
-                </div>
+function Calculator() {
+    const [state, setState] = useState(0);
+    const handleClick = (event) => {
+        setState((state) => calculate(state, event.target.innerHTML));
+        
+    };
+    return (
+        <div className='container'>
+            <form>
+                <input type="text" value={state.next || state.total || 0} />
+            </form>
+            <div className='keypad'>
+                <button type='button' className='firstline' onClick={handleClick}>AC</button>
+                <button type='button' className='firstline' onClick={handleClick}>+/-</button>
+                <button type='button' className='firstline' onClick={handleClick}>%</button>
+                <button type='button' className='extra' onClick={handleClick}>&divide;</button>
+                <button type='button' className='secondline' onClick={handleClick}>7</button>
+                <button type='button' className='secondline' onClick={handleClick}>8</button>
+                <button type='button' className='secondline' onClick={handleClick}>9</button>
+                <button type='button' className='extra' onClick={handleClick}>x</button>
+                <button type='button' className='thirdline' onClick={handleClick}>4</button>
+                <button type='button' className='thirdline' onClick={handleClick}>5</button>
+                <button type='button' className='thirdline' onClick={handleClick}>6</button>
+                <button type='button' className='extra' onClick={handleClick}>-</button>
+                <button type='button' className='fourthline' onClick={handleClick}>1</button>
+                <button type='button' className='fourthline' onClick={handleClick}>2</button>
+                <button type='button' className='fourthline' onClick={handleClick}>3</button>
+                <button type='button' className='extra' onClick={handleClick}>+</button>
+                <button type='button' className='cero' onClick={handleClick}>0</button>
+                <button type='button' className='dot' onClick={handleClick}>.</button>
+                <button type='button' className='equal' onClick={handleClick}>=</button>
             </div>
-        )
-    }
+        </div>
+    );
+
 }
 
-export default Components;
+export default Calculator;
